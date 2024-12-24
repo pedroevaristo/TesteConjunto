7a015ea890dad048082281ece6f8cef36113434d
Pedro Evaristo Dantas Zaranza <pedrodzprogramacao@gmail.com>
2024-12-23 22:22:16 -0300

    correções no configDB

--- src/main/java/configDB/InitializerDB.java
+++ src/main/java/configDB/InitializerDB.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class InitializerDB {
+	public static void initialize() {
 	try(Connection connection = ConfigDB.getConnection()){
 		
 	}catch(SQLException e){
 		e.printStackTrace();
+	}
 	}
 }
--- src/main/java/org/conndb/connproj/App.java
+++ src/main/java/org/conndb/connproj/App.java
@@ -6,6 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
-       
+       configDB.InitializerDB init = new InitializerDB();
+       init.initialize();
     }
 }
